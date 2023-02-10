b2149413ffc7b84ca2670b078da1b97be7fa5fd6
Raquel Quirino <raquelnbezerra@gmail.com>
2023-02-10 16:52:29 -0300

    set backgorundImageView

--- AppAlcoolOuGasolina/AppAlcoolOuGasolina/Home/HomeViewController.swift
+++ AppAlcoolOuGasolina/AppAlcoolOuGasolina/Home/HomeViewController.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
-
-
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "BG HOME")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addSubview(backgroundImageView)
+    }
+    
+    private func setupConstraints() {
+        
+    }
+    
 }
